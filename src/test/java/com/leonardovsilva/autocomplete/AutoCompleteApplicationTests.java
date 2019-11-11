6c3fa3d7ef18dd6384bf15d50cc20a39ad2ec68e
Leonardo Viana Silva <lvianasi@engdb.infra>
2019-11-11 18:10:29 -0300

    first commit

---
+++ src/test/java/com/leonardovsilva/autocomplete/AutoCompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardovsilva.autocomplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
